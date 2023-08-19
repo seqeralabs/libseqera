dc40155d926928f5f13b1b65dca256bd8877ee08
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-08-20 01:25:57 +0200

    Fix publish script
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- publish.sh
+++ publish.sh
@@ -5,6 +5,10 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+if [[ ! -n "$AWS_REGION" && ! -n "$AWS_DEFAULT_REGION" ]]; then
+  export AWS_DEFAULT_REGION="eu-west-1"
+fi
+
 NAME=$1
 VERSION=$(cat $NAME/VERSION)
 PUBLISH_REPO_URL=${PUBLISH_REPO_URL:-s3://maven.seqera.io/snapshots}
@@ -14,11 +18,17 @@ BASE_PATH=$(basename $BASE_URL)
 KEY=$BASE_PATH/io/seqera/$NAME/$VERSION/$NAME-$VERSION.pom
 
 echo "Publishing '$NAME-$VERSION' to $BUCKET/$KEY"
-aws s3api head-object --bucket $BUCKET --key $KEY &> /dev/null
-
-# Check the exit status of the command
-if [ $? -eq 0 ]; then
+ret=$(aws s3api head-object --bucket $BUCKET --key $KEY 2>&1) && {
+  # already exists => just a message 
   echo "NOTE: Library $NAME-$VERSION already exist - skipping publishing"
-else
-  ./gradlew $NAME:publishMavenPublicationToSeqeraRepositoryRepository
-fi
+ } || {
+  if [[ $ret == *"Not Found"* ]]; then
+    # the lib does not exist => punlish it
+    ./gradlew $NAME:publishMavenPublicationToSeqeraRepositoryRepository
+  else 
+    # print the error message
+    echo $ret >&2
+    exit 1
+  fi 
+}
+
