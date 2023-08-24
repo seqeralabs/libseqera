bd12e5e2c4b24eea09460310ecf78dedbd2036bd
munishchouhan <hrma017@gmail.com>
2023-08-24 16:01:11 +0200

    Added entrypoint

--- wave-utils/src/test/groovy/io/seqera/wave/util/DockerHelperTest.groovy
+++ wave-utils/src/test/groovy/io/seqera/wave/util/DockerHelperTest.groovy
@@ -522,8 +522,15 @@ class DockerHelperTest extends Specification {
                 /opt/spack-env /opt/spack-env
                 /opt/software /opt/software
                 /opt/._view /opt/._view
+            
             %post
-                USER hola'''.stripIndent()
-
+                mkdir -p /.singularity.d/env
+                cp -p /opt/spack-env/z10_spack_environment.sh /.singularity.d/env/91-environment.sh
+                echo "#!/usr/bin/env bash\\n\\nset -ef -o pipefail\\nsource /opt/spack-env/z10_spack_environment.sh\\nexec \\"\\$@\\"" \\
+                    >/opt/spack-env/spack_docker_entrypoint.sh && chmod a+x /opt/spack-env/spack_docker_entrypoint.sh
+                USER hola
+            
+            %runscript
+                /opt/spack-env/spack_entrypoint.sh'''.stripIndent()
     }
 }
