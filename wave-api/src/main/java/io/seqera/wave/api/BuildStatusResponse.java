a054ebbf3a62848232c60ac0752514deb69c8951
munishchouhan <hrma017@gmail.com>
2024-04-02 20:59:45 +0200

    Added BuildStatusResponse
    Signed-off-by: munishchouhan <hrma017@gmail.com>

---
+++ wave-api/src/main/java/io/seqera/wave/api/BuildStatusResponse.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024, Seqera Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.seqera.wave.api;
+
+import java.time.Duration;
+import java.time.Instant;
+
+
+/**
+ * Build status response
+ *
+ * @author Munish Chouhan <munish.chouhan@seqera.io>
+ */
+public class BuildStatusResponse {
+    public enum Status { PENDING, COMPLETED }
+
+    /** Build Id */
+    final public String id;
+
+    /** Status of image build */
+    final public Status status;
+
+    /** Build start time */
+    final public Instant startTime;
+
+    /** Duration to complete build */
+    final public Duration duration;
+
+    /** Build success status */
+    final public Boolean succeeded;
+
+    public BuildStatusResponse(String id, Status status, Instant startTime, Duration duration, Boolean succeeded) {
+        this.id = id;
+        this.status = status;
+        this.startTime = startTime;
+        this.duration = duration;
+        this.succeeded = succeeded;
+    }
+}
