ff1d95d5e0adf989389a8b299db8f784e8ef3948
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2025-02-02 10:19:58 +0100

    Add IndexSpec to ContainerInspectResponse
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- wave-api/src/main/java/io/seqera/wave/api/ContainerInspectResponse.java
+++ wave-api/src/main/java/io/seqera/wave/api/ContainerInspectResponse.java
@@ -20,15 +20,26 @@ package io.seqera.wave.api;
 import java.util.Objects;
 
 import io.seqera.wave.core.spec.ContainerSpec;
+import io.seqera.wave.core.spec.IndexSpec;
+import io.seqera.wave.model.ContainerOrIndexSpec;
 
 /**
- * Model a container request response
+ * Model a container inspect request response
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
 public class ContainerInspectResponse {
 
+    /**
+     * The spec of the specific container or {@code null} when a multi-architecture container was specified.
+     * See also {@link #index}.
+     */
     ContainerSpec container;
+
+    /**
+     * The index spec of a multi-architecture container. This attribute is mutually exclusive with {@link #container}.
+     */
+    IndexSpec index;
 
     private ContainerInspectResponse() {
 
@@ -38,8 +49,21 @@ public class ContainerInspectResponse {
         this.container = container;
     }
 
+    public ContainerInspectResponse(IndexSpec index) {
+        this.index = index;
+    }
+
+    public ContainerInspectResponse(ContainerOrIndexSpec spec) {
+        this.container = spec.getContainer();
+        this.index = spec.getIndex();
+    }
+
     public ContainerSpec getContainer() {
         return container;
+    }
+
+    public IndexSpec getIndex() {
+        return index;
     }
 
     @Override
@@ -47,18 +71,21 @@ public class ContainerInspectResponse {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
         ContainerInspectResponse that = (ContainerInspectResponse) object;
-        return Objects.equals(container, that.container);
+        return Objects.equals(container, that.container)
+                && Objects.equals(index, that.index)
+                ;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(container);
+        return Objects.hash(container, index);
     }
 
     @Override
     public String toString() {
         return "ContainerInspectResponse{" +
                 "container=" + container +
+                "index=" + index +
                 '}';
     }
 }
